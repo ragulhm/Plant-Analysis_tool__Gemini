f0dbe62ba80fb001d113f0594039cc801d4c3533
Ragul M <116007353+ragulhm@users.noreply.github.com>
2025-09-29 22:10:27 +0530

    Update app.js

--- Mern Plant-analaysis/app.js
+++ Mern Plant-analaysis/app.js
@@ -32,7 +32,7 @@ app.post("/analyze", upload.single("image"), async (req, res) => {
     });
 
     // Use the Gemini model to analyze the image
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
     const result = await model.generateContent([
       "Analyze this plant image and provide detailed analysis of its species, health, and care recommendations, its characteristics, care instructions, and any interesting facts. Please provide the response in plain text without using any markdown formatting.",
       {
